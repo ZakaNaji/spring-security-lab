dac49504eef515e665bd8c6e7fee174cc1b60c61
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-12-14 14:33:33 +0100

    feat: jwt service - shorter jwt life span

--- src/main/java/com/znaji/securitylab/security/JwtService.java
+++ src/main/java/com/znaji/securitylab/security/JwtService.java
@@ -3,23 +3,19 @@ package com.znaji.securitylab.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.util.Base64;
-import java.util.Collection;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
     private static final String SECRET = "M1FhS3ZyZVZ6b3pQbXJjRkR2b0R4QkJ1bFZ0RkR1R1A3a1ZxZQ==";
-    private static final long EXPIRATION = 60 * 60 * 1000; // 1 hour
+    private static final long EXPIRATION = 10 * 60 * 1000; // 10 min
 
     private final Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET));
 
