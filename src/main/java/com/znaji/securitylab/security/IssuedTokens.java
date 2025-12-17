58ab67371e2e841bcee1ffbcdf793fd6d1e8c6e6
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-12-17 19:30:38 +0100

    feat(refresh token rotation): dtos - RotationResult & IssuedTokens

---
+++ src/main/java/com/znaji/securitylab/security/IssuedTokens.java
@@ -0,0 +1,3 @@
+package com.znaji.securitylab.security;
+
+public record IssuedTokens(String accessToken, String refreshToken) {}
---
+++ src/main/java/com/znaji/securitylab/security/RotationResult.java
@@ -0,0 +1,3 @@
+package com.znaji.securitylab.security;
+
+public record RotationResult(String username, String familyId, String newRefreshToken) {}
